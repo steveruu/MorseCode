e8fcd16a1357f1c496ca6b30bc69a829247f4ec5
steveruu <prchalek@gmail.com>
2024-02-09 10:46:27 +0100

    +encode()

--- MorseCode/MorseCode/Morse.cs
+++ MorseCode/MorseCode/Morse.cs
@@ -1,5 +1,96 @@
+using Microsoft.Extensions.Logging.Console;
+
 namespace MorseCode; 
 
 public class Morse {
+    protected Dictionary<string, string> Slovnik = new() {
+        {"a", ".-"}, 
+        {"á", ".-"}, 
+        {"ą", ".-"},
+        {"b", "-..."},
+        {"c", "-.-."},
+        {"č", "-.-."},
+        {"d", "-.."},
+        {"ď", "-.."},
+        {"e", "."},
+        {"é", "."},
+        {"ě", "."},
+        {"ę", "."},
+        {"f", "..-."},
+        {"g", "--."},
+        {"h", "...."},
+        {"ch", "----"},
+        {"i", ".."},
+        {"í", ".."},
+        {"j", ".---"},
+        {"k", "-.-"},
+        {"l", ".-.."},
+        {"m", "--"},
+        {"n", "-."},
+        {"ň", "-."},
+        {"o", "---"},
+        {"ó", "---"},
+        {"p", ".--."},
+        {"q", "--.-"},
+        {"r", ".-."},
+        {"ř", ".-."},
+        {"s", "..."},
+        {"š", "..."},
+        {"t", "-"},
+        {"ť", "-"},
+        {"u", "..-"},
+        {"ů", "..-"},
+        {"ú", "..-"},
+        {"v", "...-"},
+        {"w", ".--"},
+        {"x", "-..-"},
+        {"y", "-.--"},
+        {"ý", "-.--"},
+        {"z", "--.."},
+        {"ž", "--.."},
+        {"1", ".----"},
+        {"2", "..---"},
+        {"3", "...--"},
+        {"4", "....-"},
+        {"5", "....."},
+        {"6", "-...."},
+        {"7", "--..."},
+        {"8", "---.."},
+        {"9", "----."},
+        {"0", "-----"},
+        {" ", "/"}
+    };
     
+    // predelat š na s, č na c atd.
+    
+
+    public string Encode(string text) {
+        string zasifrovano = "";
+        List<string> znaky = new();
+        
+        foreach (char znak in text) {
+            znaky.Add(znak.ToString().ToLower());
+        }
+
+        for (int i = 0; i < znaky.Count; i++) {
+            if (znaky[i] == "c" && znaky[i + 1] == "h") {
+                znaky[i] = "ch";
+                znaky.RemoveAt(i + 1);
+            }
+            
+            if (Slovnik.ContainsKey(znaky[i])) {
+                zasifrovano += Slovnik[znaky[i]] + " ";
+            }
+        }
+        
+        return zasifrovano;
+    }
+
+    public string Decode(string text) {
+        string desifrovano = "";
+        
+        string[] symbols = text.Split('/');
+
+        return "A";
+    }
 }
--- MorseCode/MorseCode/Program.cs
+++ MorseCode/MorseCode/Program.cs
@@ -1,6 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+namespace MorseCode {
+    internal class Program {
+        private static void Main(string[] args) {
+            Morse morse = new();
+            Console.WriteLine(morse.Encode("chello world"));
+            Console.WriteLine(morse.Encode("Budiž k ničemu"));
+            Console.WriteLine(morse.Encode("Běží liška k Táboru, nese pytel zázvoru"));
+        }
+    }
+}
